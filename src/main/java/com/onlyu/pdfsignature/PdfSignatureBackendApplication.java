4acca17e4eebd3895f520ff99bd208e0b81095f8
Ali REKIK <medali.rekik.ing@gmail.com>
2026-05-13 19:47:53 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/onlyu/pdfsignature/PdfSignatureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.onlyu.pdfsignature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfSignatureBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfSignatureBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlyu/pdfsignature/PdfSignatureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlyu.pdfsignature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfSignatureBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
